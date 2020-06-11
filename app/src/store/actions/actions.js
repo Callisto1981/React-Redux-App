ca83882cb3f53b80a7d254da0bb96d6a7c86463a
Wayne <wabbruscatoorr@gmail.com>
2020-06-11 18:42:58 -0400

    added endpoint to actions file

--- app/src/store/actions/actions.js
+++ app/src/store/actions/actions.js
@@ -1,5 +1,11 @@
+import axios from "axios";
+
 export const fetchCountry = () => {
   return dispatch => {
     dispatch({ type: "FETCH_COUNTRY" });
+    axios
+      .get(`https://restcountries.eu/rest/v2/name/${countryName}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
 };
